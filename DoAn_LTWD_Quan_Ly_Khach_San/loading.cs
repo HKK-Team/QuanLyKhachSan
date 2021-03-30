31956d211c8d55993546eacab1db548630f3d068
tieuthubey <58809781+MinhKhanh693@users.noreply.github.com>
2021-03-30 11:53:57 +0700

    up 2 form loading

---
+++ DoAn_LTWD_Quan_Ly_Khach_San/loading.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAn_LTWD_Quan_Ly_Khach_San
+{
+    public partial class loading : Form
+    {
+        public loading()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int star = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            star += 1;
+            circularProgressBar1.Value = star;
+            circularProgressBar1.Text = Convert.ToString(star);
+            if(star == 101 )
+            {
+                star = 0;
+                timer1.Stop();
+                loading_2 h = new loading_2();
+                this.Hide();
+                h.Show();
+            }
+        }
+
+        private void loading_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
---
+++ DoAn_LTWD_Quan_Ly_Khach_San/loading_2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAn_LTWD_Quan_Ly_Khach_San
+{
+    public partial class loading_2 : Form
+    {
+        public loading_2()
+        {
+            InitializeComponent();
+        }
+
+        private void loading_2_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        int star = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            star += 5;
+            progressBar1.Value = star;
+            if(star == 100)
+            {
+                star = 0;
+                timer1.Stop();
+                DangNhap h = new DangNhap();
+                this.Hide();
+                h.Show();
+            }
+        }
+    }
+}
